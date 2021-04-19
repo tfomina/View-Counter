8edb651c176406f89daffb7b8680bd9f4abc9593
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-19 18:18:03 +0300

    Переделала на хранение счетчика в json файле

--- routes/counter.js
+++ routes/counter.js
@@ -1,28 +1,52 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
-const counter = {};
+const filePath = path.join(__dirname, "..", "counter.json");
+
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "{}", "utf8");
+}
 
 // увеличить счётчик
 router.post("/:bookId/incr", (req, res) => {
   const { bookId } = req.params;
-  if (!(bookId in counter)) {
-    counter[bookId] = 1;
-  } else {
-    counter[bookId] = ++counter[bookId];
-  }
-  res.json("Ok");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) throw err;
+
+    const counter = JSON.parse(data);
+
+    if (!(bookId in counter)) {
+      counter[bookId] = 1;
+    } else {
+      counter[bookId] = ++counter[bookId];
+    }
+
+    fs.writeFileSync(filePath, JSON.stringify(counter), "utf8", (err) => {
+      if (err) throw err;
+    });
+
+    res.json("Ok");
+  });
 });
 
 // получить значение счётчика
 router.get("/:bookId", (req, res) => {
   const { bookId } = req.params;
 
-  if (!(bookId in counter)) {
-    res.status(404).json("Not found");
-  } else {
-    res.json({ counter: counter[bookId] });
-  }
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) throw err;
+
+    const counter = JSON.parse(data);
+
+    if (!(bookId in counter)) {
+      res.status(404).json("Not found");
+    } else {
+      res.json({ counter: counter[bookId] });
+    }
+  });
 });
 
 module.exports = router;

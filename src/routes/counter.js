52f9e0b3f78bef00aab4bdd08fc37062825f8ccd
Tatyana Fomina <tfomina@logrocon.com>
2021-04-21 15:59:08 +0300

    Переделала на хранение в локальной папке

--- src/routes/counter.js
+++ src/routes/counter.js
@@ -1,20 +1,48 @@
 const express = require("express");
-const redis = require("redis");
-
-const STORAGE_URL = process.env.STORAGE_URL;
-const client = redis.createClient(`redis://${STORAGE_URL}`);
-
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
-// увеличить счётчик по id книги и вернуть значение
+const folder = "C:/counter-storage";
+const filePath = `${folder}/counter.json`;
+
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, "{}", "utf8");
+}
+
+// увеличить счётчик по id книги
+router.post("/:bookId/incr", (req, res) => {
+  const { bookId } = req.params;
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) throw err;
+
+    const counter = JSON.parse(data);
+
+    if (!(bookId in counter)) counter[bookId] = 0;
+    counter[bookId] += 1;
+
+    fs.writeFileSync(filePath, JSON.stringify(counter), "utf8", (err) => {
+      if (err) throw err;
+    });
+
+    res.json("Ok");
+  });
+});
+
+// получить значение счётчика по id книги
 router.get("/:bookId", (req, res) => {
   const { bookId } = req.params;
 
-  client.incr(bookId, (err, rep) => {
-    if (err) {
-      res.statusCode(500).json(err);
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) throw err;
+
+    const counter = JSON.parse(data);
+
+    if (!(bookId in counter)) {
+      res.status(404).json("Not found");
     } else {
-      res.json(rep);
+      res.json(counter[bookId]);
     }
   });
 });

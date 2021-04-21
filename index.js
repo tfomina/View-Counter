3b61e2c12e57b80e81c7aa50cc50d04ef4c5e9bb
Татьяна Фомина <tfomina@logrocon.local>
2021-04-21 10:44:37 +0300

    добавила app.use(cors()) в index.js

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
 app.use("/counter", counterRouter);
 
 app.use(notFoundMiddleware);

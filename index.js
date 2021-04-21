cf2cfb72a2c3605ad2dd130f7c60e7a544ac3e5d
Tatyana Fomina <tfomina@logrocon.com>
2021-04-21 11:41:31 +0300

    Исправление ошибок

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/counter", counterRouter);
 
 app.use(notFoundMiddleware);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

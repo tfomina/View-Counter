c6a0d43039b368d6140469876889e5e0b385c1ee
Tatyana Fomina <tfomina@logrocon.com>
2021-04-22 16:30:59 +0300

    Доработка

--- src/routes/counter.js
+++ src/routes/counter.js
@@ -45,7 +45,7 @@ router.get("/:bookId", (req, res) => {
     if (!(bookId in counter)) {
       res.status(404).json("Not found");
     } else {
-      res.json({ test: counter[bookId] });
+      res.json({ counter: counter[bookId] });
     }
   });
 });
